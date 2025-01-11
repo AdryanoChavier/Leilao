193a5525162822ea5934c721e81b8c6dbaae49b3
Adryano Chavier <adryano.chavieroficial@gmail.com>
2025-01-11 15:46:21 -0300

    Preparando pra integração com Postgres

---
+++ src/AuctionService/Data/LeilaoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class LeilaoDbContext : DbContext
+    {
+        public LeilaoDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+        public DbSet<Leilao> Leiloes { get; set; }
+    }
+}
---
+++ src/AuctionService/Data/Migrations/20250111184429_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leiloes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReservaPreco = table.Column<int>(type: "integer", nullable: false),
+                    Vendedor = table.Column<string>(type: "text", nullable: false),
+                    Ganhador = table.Column<string>(type: "text", nullable: false),
+                    QuantidadeVendida = table.Column<int>(type: "integer", nullable: true),
+                    AtualOferta = table.Column<int>(type: "integer", nullable: true),
+                    CreateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LeilaoEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leiloes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false),
+                    Ano = table.Column<int>(type: "integer", nullable: false),
+                    Cor = table.Column<string>(type: "text", nullable: false),
+                    Quilometragem = table.Column<int>(type: "integer", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    LeilaoId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Leiloes_LeilaoId",
+                        column: x => x.LeilaoId,
+                        principalTable: "Leiloes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_LeilaoId",
+                table: "Items",
+                column: "LeilaoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Leiloes");
+        }
+    }
+}
