c64db90d6b72e37c2ba6d20a35eb8281383c4009
Adryano Chavier <adryano.chavieroficial@gmail.com>
2025-01-14 22:06:29 -0300

    Adicona stringConnectMongo

---
+++ src/PesquisaService/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Entities;
+
+namespace PesquisaService.Models
+{
+    public class Item : Entity
+    {
+        public int ReservaPreco { get; set; } = 0;
+        public string Vendedor { get; set; }
+        public string Ganhador { get; set; }
+        public int QuantidadeVendida { get; set; }
+        public int AtualOferta { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime LeilaoEnd { get; set; }
+        public string Status { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+        public int Quilometragem { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- src/PesquisaService/Program.cs
+++ src/PesquisaService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using PesquisaService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("PesquisaDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration
+    .GetConnectionString("MongoDbConnect")));
+
+await DB.Index<Item>()
+    .Key(x => x.Marca, KeyType.Text)
+    .Key(x => x.Modelo, KeyType.Text)
+    .Key(x => x.Cor, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
