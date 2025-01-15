e77be7e17538e5baca0c4fabda46d015e4085f6b
adryano.chavier <adryano.chavier@senac.br>
2025-01-15 16:07:59 -0300

    Atualização de requisição Http entre micros serviços

--- src/PesquisaService/Program.cs
+++ src/PesquisaService/Program.cs
@@ -1,15 +1,15 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
 using PesquisaService.Data;
-using PesquisaService.Models;
 using PesquisaService.Services;
+using Polly;
+using Polly.Extensions.Http;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<LeilaoSvcHttpClient>();
+builder.Services.AddHttpClient<LeilaoSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -19,14 +19,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
 
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
